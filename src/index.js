89fd1c39f6f0b364796df422fb54a6037bd5d4bc
Gion-Andri Cantieni <gion-andri@gmx.ch>
2023-10-09 09:31:53 +0200

    feat: add new idioms

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const loadIniFile = require('read-ini-file')
 const handlebars = require('handlebars');
 const zipDir = require('zip-dir');
 
-const validIdioms = ['surmiran', 'sutsilv'];
+const validIdioms = ['puter', 'rumgr', 'surmiran', 'sutsilv', 'vallader'];
 
 async function pack() {
     const idiom = process.argv[3];
